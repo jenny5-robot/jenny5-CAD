285e82805751106e87751e097d3c15a7533df1ae
Mihai Oltean <mihai.oltean@gmail.com>
2018-11-24 13:15:04 +0200

    picture generated script updated

--- bat/robot.bat
+++ bat/robot.bat
@@ -1,1 +1,1 @@
-"c:\Program Files\OpenSCAD\openscad.exe" -o jenny5.png --camera=240,90,460,60,0,295,4000 --imgsize=480,700 ..\robot\jenny5.scad
+"c:\Program Files\OpenSCAD\openscad.exe" -o jenny5.png --camera=240,90,400,60,0,285,4000 --imgsize=480,700 ..\robot\jenny5.scad
